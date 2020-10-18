c95796df926f260d4ef531083ae9dc5c6ac9024f
Tomohiro4196 <tomo4196.w@gmail.com>
2020-10-18 13:43:28 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_112502) do
+ActiveRecord::Schema.define(version: 2020_10_16_104910) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
