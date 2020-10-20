921ac3730961358c68c7ec7663f57c3e29e49a54
Tomohiro4196 <tomo4196.w@gmail.com>
2020-10-20 17:34:52 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,31 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    #params内のroom_idを取得
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+       #@room.message.newで現在のチャットルームに紐づいたメッセージのインスタンスを作成
+    #privateメソッド(message_params)で内容を指定
+     if  @message.save
+       #saveメソッドで保存→保存できたらメッセージ画面へリダイレクト
+        redirect_to room_messages_path(@room.id)
+     else 
+        render :index
+        #元のページに戻る
+     end
+ 
+  end
+
+
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    #contentのみを保存。
+    #ログインしているユーザーのIDをmergeして、current_user.idのみが紐づいているmessageを受け取る
   end
 end
